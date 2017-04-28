b28770cc1994ec204544c4b14a78938ffeff5f43
Bacalhau <joao.bacalhau@i9lab.com>
2017-04-27 22:06:58 -0300

    Express, login page, recovery page, express routes for get, and express routes for post on login and recovery. Angular sending POST on submit of login and recovery.

--- src/app.js
+++ src/app.js
@@ -1,27 +1,73 @@
 var fs = require('fs');
-var http = require('http');
 var https = require('https');
+var jwt    = require('jsonwebtoken');
+var express = require('express');
+var bodyParser = require('body-parser')
+var configParameter =require('./config.js');
 var privateKey  = fs.readFileSync('privatekey.key', 'utf8');
 var certificate = fs.readFileSync('certificate.crt', 'utf8');
 
+
+var port = process.env.PORT || 8443;
+
 var credentials = {key: privateKey, cert: certificate};
-var express = require('express');
+
 var app = express();
 
 app.set('view engine', 'ejs');
 
-// your express configuration here
+
+var LoggedUsers = [];
+
+
 app.use('/assets', express.static(__dirname + '/public'));
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/', function(req,res) {
     res.render('index');
 });
 
+app.get('/admin', function(req,res) {
+    res.render('admin');
+});
+
+app.get('/login', function(req,res) {
+    res.render('login');
+});
+
+app.get('/forgotpassword', function(req,res) {
+    res.render('forgotpassword');
+});
+
+//Hidden URL
 app.get('/bye', function(req,res) {
     res.send('bye');
 });
 
+app.post('/login',function(req,res){
+
+    console.log("POST RECEIVED");
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.setHeader('Content-Type', 'application/json');
+    res.end();
+
+});
+
+app.post('/forgotpassword',function(req,res){
+
+    console.log("POST RECEIVED PASSWORD FORGOT");
+    console.log(req.body.username);
+    res.setHeader('Content-Type', 'application/json');
+    res.end();
+
+});
+
 var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(8443);
+httpsServer.listen(port);
 
--- src/public/appc.js
+++ src/public/appc.js
@@ -1,39 +1,102 @@
-var myApp = angular.module('myApp', ['ngRoute','chart.js']);
+var myApp = angular.module('myApp', ['ngRoute']);
+ 
 
 myApp.config(function ($routeProvider) {
     
-    
-    $routeProvider    
-    .when('/', {
-        templateUrl: '../views/login.html',
-        controller: 'mainController'
-    })
-    
+   
+    $routeProvider        
     .when('/admin', {
-        templateUrl: 'pages/admin.html',
+        templateUrl: './admin',
         controller: 'adminController'
     })
-        .when('/forgotpassword', {
-        templateUrl: 'pages/forgotpassword.html',
+    .when('/', {
+        templateUrl: './login',
+        controller: 'mainController'
+    })
+    .when('/forgotpassword', {
+        templateUrl: './forgotpassword',
         controller: 'forgotpasswordController'
     })
+
    
     
 });
 
 
-myApp.controller('mainController', ['$scope', function($scope,$log) {
-    
+myApp.controller('mainController', ['$scope','$http','$log', function($scope,$http,$log) {
+   
+    $scope.email ="";
+    $scope.password = "";
+    $scope.submit = function(form) { 
+
+        
+        $log.log($scope);
+        //$scope.isDisabled=true;
+        var req = {
+            method: 'POST',
+            url: 'https://localhost:8443/login/',
+            headers: {
+                        'Content-Type': 'application/json'
+                    },
+            data: { 
+                username: $scope.email,
+                password: $scope.password
+
+                  }
+                }
+
+        $http(req).then(function(data, status, headers, config){
+
+            $log.log('success');
+        }, function(data, status, headers, config){
+
+           $log.log('error');
+        });
+
+    }
     
     
 }]);
 
 myApp.controller('adminController', ['$scope', function($scope) {
     
-
+ 
 }]);
 
 
-myApp.controller('forgotpasswordController', ['$scope', function($scope) {
-    
+myApp.controller('forgotpasswordController', ['$scope','$http','$log', function($scope,$http,$log) {
+  
+
+    $scope.email ="";    
+    $scope.submit = function(form) { 
+
+        
+        $log.log($scope);
+        $scope.isDisabled=true;
+        var req = {
+            method: 'POST',
+            url: 'https://localhost:8443/forgotpassword/',
+            headers: {
+                        'Content-Type': 'application/json'
+                    },
+            data: { 
+                username: $scope.email                
+
+                  }
+                }
+
+        $http(req).then(function(data, status, headers, config){
+
+            $log.log('success');
+            $scope.isDisabled=false;
+        }, function(data, status, headers, config){
+
+           $log.log('error');
+           $scope.isDisabled=false;
+        });
+
+    }
+
+
+
 }]);
